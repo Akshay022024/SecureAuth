8b088ff6a012fc4aa88067dc6df8064a67b4eb6a
Nandigam Akshay <155251290+Akshay022024@users.noreply.github.com>
2025-02-10 13:14:11 +0530

    Update Register.jsx

--- AuthUI/ui/src/components/Register.jsx
+++ AuthUI/ui/src/components/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_BASE_URL = "https://localhost:7097"; // Make sure this matches your backend
+const API_BASE_URL = "https://localhost:7097"; 
 
 function Register({ setUser }) {
   const navigate = useNavigate();
